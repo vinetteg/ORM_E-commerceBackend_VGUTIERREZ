6988a9ec9215a11457c2c49cdcb959ea085061f8
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-07-19 22:14:36 -0700

    Working on models

---
+++ models/Category.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // Manually define the primary key
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  through: {
+    model:
+  }
+})
+
+// Categories have many Products
+Category.belongsToMany(Product, {
+  through: {
+    model:
+
+  }
+}) 
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
